9e0bf0d473dc3931f32e19386f9ffa1c101bba6a
Anvar Umarov <arpanetus@gmail.com>
2018-09-04 04:36:12 +0600

    fixed grammar mistakes, changed sciprt.js behaviour

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 let email = document.getElementById("email");
 let submit = document.getElementById("submit");
 let xhr = new XMLHttpRequest();
-
-
+let type = 'iitu';
+let ip = '';
+let port = '';
 
 let send = () => {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  console.log(email.value);
 
   if (!re.test(email.value)) {
     alert("Please, enter the correct email!");
     return;
   }
   xhr.onreadystatechange = () => {
-    if (this.readyState == 4 && this.status == 200) {
+    if (xhr.readyState == 4 && xhr.status == 200) {
       response = xhr.responseText;
       responseObject = JSON.parse(response);
-      if (responseObject['state'] === 'wrong_email') {
-        alert("Please, enter the correct email!");
-      } else if (responseObject['state'] === 'ok') {
+      console.log(responseObject);
+      if (responseObject['emailValidity'] === false || responseObject['typeValidity'] === false) {
+        alert("Please, enter the correct email and type!");
+      } else {
         alert("Thank you, we'll contact asap!");
-      }
-      else {
-        alert("Something is wrong, try again!");
       }
     }
   }
-  xhr.open('POST', 'http://ip.com/icecream', true);
+  xhr.open('POST', ip + ':'+port+'/icecream', true);
   xhr.setRequestHeader("Content-type", "application/json");
-  xhr.send('{\'email\':\'' + email.value + '\'}');
+  xhr.send(JSON.stringify({ 'email': email.value, 'type': type }));
 
 }
 
